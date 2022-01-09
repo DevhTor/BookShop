fad89bdf51ee7728abbac3de0ee745df92b01a98
hectoristy <34715623+hectoristy@users.noreply.github.com>
2022-01-08 23:20:41 -0400

    administrador comenzado

---
+++ admin/template/header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <?php 
+    
+    $url="http://" . $_SERVER['HTTP_HOST'] . "/BookShop";
+    
+    ?>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link active" href="#">Administrador <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Home</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/admin/section/books.php">Libros</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/admin/section/closeSession.php">Cerrar Sesion</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>">Ver Sitio Web</a>
+        </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <br>
+        <div class="row">
---
+++ template/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookShop</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand navbar-light bg-primary">
+
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">BookShop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="books.php">Libros</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="we.php">Nosotros</a>
+            </li>
+        </ul>
+
+    </nav>
+
+    <div class="container">
+        <br>
+        <div class="row"/>
+        
