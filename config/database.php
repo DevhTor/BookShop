0a44a609d69da4cb9b3f7de2311a0fa5a9f681c6
hectoristy <34715623+hectoristy@users.noreply.github.com>
2022-02-03 22:42:02 -0400

    contraseña corregida

--- config/database.php
+++ config/database.php
@@ -3,10 +3,11 @@
 $host = "bdmnt4lojlwmhmh84sde-mysql.services.clever-cloud.com";
 $dbName = "bdmnt4lojlwmhmh84sde";
 $user = "u6vfjlrvzxku2itk";
-$pass = "u6vfjlrvzxku2itk";
+$pass = "INrp9Hw0vOOgd77X6w9Z";
 
 try {
     $conn = new PDO("mysql:host=$host; dbname=$dbName", $user, $pass);
+    
    
 } catch (Exception $err) {
     echo "error: " . $err->getMessage();
