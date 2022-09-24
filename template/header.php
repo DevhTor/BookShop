46436d3fcd21b645d8f99fbfa0d8b9dd68ce2049
root <root@localhost>
2022-09-24 08:05:54 -0400

    bootstrap file

--- template/header.php
+++ template/header.php
@@ -13,6 +13,7 @@ include("../config/Server.php")
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookShop</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+		<link rel="stylesheet" href="../css/bootstrap.css">
     </head>
 
   </head>
@@ -20,12 +21,12 @@ include("../config/Server.php")
   <body>
     <nav class=" navbar navbar-expand-md navbar-light bg-primary">
       <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" data-target="#menu" data-toggle="collapse" type="button">
           <span class="navbar-toggler-icon"></span>
         </button>
       
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse" id="menu">
+          <ul class=" navbar-nav mr-auto">
             <li class="nav-item">
               <a class="nav-link text-white " href="<?php echo $urlBase ?>/index.php">BookShop</a>
             </li>
@@ -65,7 +66,6 @@ include("../config/Server.php")
       </div>
     </nav>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+		<script src="../script/boostrap.js"></script>
   </body>
 </html>
